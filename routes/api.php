97419ebca74d8a2b69b945052b121f6106f93b57
ckliao-nccu <ckliao@g.nccu.edu.tw>
2022-09-29 21:14:55 +0800

    Pretty

--- routes/api.php
+++ routes/api.php
@@ -18,12 +18,9 @@ use Illuminate\Http\Request;
 // });
 
 Route::group([
-
     'middleware' => 'api',
     'prefix' => 'auth'
-
 ], function ($router) {
-
     Route::post('login', 'AuthController@login');
     Route::post('logout', 'AuthController@logout');
     Route::post('refresh', 'AuthController@refresh');
