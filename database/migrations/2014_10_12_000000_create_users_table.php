db89c80a8f7a363341462e50955c8ecc153068df
ckliao-nccu <ckliao@g.nccu.edu.tw>
2022-09-30 09:29:06 +0800

    Update: user column

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,8 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('name')->comment('名稱');
+            $table->string('phone')->comment('手機號碼');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
